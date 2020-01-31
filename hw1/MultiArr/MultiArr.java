c71d6f980d07f5b7321ea030b040722019be9b3d
Aniruddh Khanwale <akhanwale@berkeley.edu>
2020-01-31 11:41:00 -0800

    Finished MultiArr Questions

--- hw1/MultiArr/MultiArr.java
+++ hw1/MultiArr/MultiArr.java
@@ -1,7 +1,6 @@
-/** Multidimensional array 
- *  @author Zoe Plaxco
+/** Multidimensional array
+ *  @author Aniruddh Khanwale
  */
-
 public class MultiArr {
 
     /**
@@ -14,7 +13,8 @@ public class MultiArr {
     Columns: 4
     */
     public static void printRowAndCol(int[][] arr) {
-        //TODO: Your code here!
+        System.out.println("Rows: " + arr.length);
+        System.out.println("Columns: " + arr[0].length);
     } 
 
     /**
@@ -22,14 +22,31 @@ public class MultiArr {
     @return maximal value present anywhere in the 2d array
     */
     public static int maxValue(int[][] arr) {
-        //TODO: Your code here!
-        return 0;
+        int max = Integer.MIN_VALUE;
+        for(int row = 0; row < arr.length; row += 1){
+            for(int col = 0; col < arr[row].length; col +=1){
+                int val = arr[row][col];
+                if(val > max){
+                    max = val;
+                }
+            }
+        }
+        return max;
     }
 
     /**Return an array where each element is the sum of the 
     corresponding row of the 2d array*/
     public static int[] allRowSums(int[][] arr) {
-        //TODO: Your code here!!
-        return null;
+        int[] sums = new int[arr.length];
+        for(int row = 0; row < arr.length; row += 1){
+            int rowSum = 0;
+            for(int col = 0; col < arr[row].length; col +=1){
+                rowSum += arr[row][col];
+            }
+            sums[row] = rowSum;
+        }
+        //System.out.println(sums);
+        return sums;
+
     }
 }
