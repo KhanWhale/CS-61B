464e70ae0b23379a39497cbd68d8a59a044cb658
Aniruddh Khanwale <akhanwale@berkeley.edu>
2020-02-03 21:16:07 -0800

    Fixed CoumpoundIntTests

--- hw1/CompoundInterest/CompoundInterestTest.java
+++ hw1/CompoundInterest/CompoundInterestTest.java
@@ -9,25 +9,25 @@ public class CompoundInterestTest {
 
         assertEquals(0, 0); */
         assertEquals(9, CompoundInterest.numYears(2029));
-        assertEquals(5, CompoundInterest.numYears(2024));
-        assertEquals(100, CompoundInterest.numYears(2119));
-        assertEquals(38, CompoundInterest.numYears(2057));
+        assertEquals(4, CompoundInterest.numYears(2024));
+        assertEquals(99, CompoundInterest.numYears(2119));
+        assertEquals(37, CompoundInterest.numYears(2057));
     }
 
     @Test
     public void testFutureValue() {
         double tolerance = 0.01;
         assertEquals(11.2, CompoundInterest.futureValue(10, 12, 2021), tolerance);
-        assertEquals(7.744, CompoundInterest.futureValue(10, -12, 2021), tolerance);
-        assertEquals(57391.4, CompoundInterest.futureValue(53000, 1, 2027), tolerance);
+        assertEquals(8.8, CompoundInterest.futureValue(10, -12, 2021), tolerance);
+        assertEquals(56823.17, CompoundInterest.futureValue(53000, 1, 2027), tolerance);
     }
 
     @Test
     public void testFutureValueReal() {
         double tolerance = 0.01;
         assertEquals(10.864, CompoundInterest.futureValueReal(10, 12, 2021, 3), tolerance);
-        assertEquals(6.98896, CompoundInterest.futureValueReal(10, -12, 2021, 5), tolerance);
-        assertEquals(114355.97035073, CompoundInterest.futureValueReal(53000, 1, 2027, -9), tolerance);
+        assertEquals(8.36, CompoundInterest.futureValueReal(10, -12, 2021, 5), tolerance);
+        assertEquals(103874.98, CompoundInterest.futureValueReal(53000, 1, 2027, -9), tolerance);
     }
 
 
@@ -35,8 +35,8 @@ public class CompoundInterestTest {
     public void testTotalSavings() {
         double tolerance = 0.01;
         assertEquals(10500, CompoundInterest.totalSavings(5000, 2021, 10), tolerance);
-        assertEquals(22067.5, CompoundInterest.totalSavings(7000, 2021, 5), tolerance);
-        assertEquals(19839.984343441, CompoundInterest.totalSavings(1257, 2029, 7), tolerance);
+        assertEquals(14350, CompoundInterest.totalSavings(7000, 2021, 5), tolerance);
+        assertEquals(17367.275, CompoundInterest.totalSavings(1257, 2029, 7), tolerance);
 
     }
 
