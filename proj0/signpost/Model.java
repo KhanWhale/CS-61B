4c77a08b60ddadc5d7dc93a91d31c0b95d03d7c5
Aniruddh Khanwale <akhanwale@berkeley.edu>
2020-02-11 08:25:59 -0800

    Fixed test00?

--- proj0/signpost/Model.java
+++ proj0/signpost/Model.java
@@ -632,9 +632,9 @@ class Model implements Iterable<Model.Sq> {
                     sq._head = this._head;
                 }
                 if(thisChanged){
-                    releaseGroup(this.group());
+                    releaseGroup(this._group);
                 }else if(s1Changed){
-                    releaseGroup(s1.group());
+                    releaseGroup(s1._group);
                 }
                 if(this.sequenceNum() == 0 && s1.sequenceNum() == 0){
                     this.head()._group = joinGroups(this._group, s1._group);
