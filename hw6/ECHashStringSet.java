57c3be5f69563042d0e36bce31a6acbcfb28f22a
Aniruddh Khanwale <akhanwale@berkeley.edu>
2020-03-22 16:04:17 -0700

    Initial implementation of ECHash

--- hw6/ECHashStringSet.java
+++ hw6/ECHashStringSet.java
@@ -1,4 +1,7 @@
+import com.sun.xml.internal.xsom.impl.scd.Iterators;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
@@ -8,21 +11,38 @@ import java.util.List;
 class ECHashStringSet implements StringSet {
     @Override
     public void put(String s) {
-        Integer myHash = s.hashCode();
-        if (myHash < 0) {
-            myHash = (s.hashCode() & 0x7fffffff) % 3;
-        } else {
-            _myHashTable.put(myHash, s);
+        boolean full = true;
+        for(int i = 0; i < _myHashTable.length; i += 1) {
+            if(_myHashTable[i] == null) {
+                full = false;
+                break;
+            }
         }
+        if (full) {
+            ArrayList[] _temp = _myHashTable;
+            _myHashTable = new ArrayList[_temp.length * 2];
+            for (int i = 0; i < _temp.length; i += 1) {
+                _myHashTable[i] = _temp[i];
+            }
+        }
+            int myHash = s.hashCode();
+            if (myHash < 0) {
+                myHash = (s.hashCode() & 0x7fffffff) % _myHashTable.length;
+            } else {
+                myHash = s.hashCode() % _myHashTable.length;
+            }
+            _myHashTable[myHash].add(s);
     }
 
     @Override
     public boolean contains(String s) {
-        Integer myHash = s.hashCode();
+        int myHash = s.hashCode();
         if (myHash < 0) {
-            myHash = (s.hashCode() & 0x7fffffff) % 3;
+            myHash = (s.hashCode() & 0x7fffffff) % _myHashTable.length;
+        } else {
+            myHash = s.hashCode() % _myHashTable.length;
         }
-        return _myHashTable.containsKey(myHash);
+        return _myHashTable[myHash].contains(s);
     }
 
     @Override
@@ -33,5 +53,5 @@ class ECHashStringSet implements StringSet {
     public ECHashStringSet () {
 
     }
-    private HashMap<Integer, String> _myHashTable = new HashMap<Integer, String>();
+    private ArrayList[] _myHashTable = new ArrayList[4];
 }
