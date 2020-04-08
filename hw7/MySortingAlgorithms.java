5b491e50782b0a0ab715a49817b821848e06e8cb
Aniruddh Khanwale <akhanwale@berkeley.edu>
2020-04-07 23:32:03 -0700

    Finished insertion, selection, implementing merge

--- hw7/MySortingAlgorithms.java
+++ hw7/MySortingAlgorithms.java
@@ -42,7 +42,13 @@ public class MySortingAlgorithms {
     public static class InsertionSort implements SortingAlgorithm {
         @Override
         public void sort(int[] array, int k) {
-            // FIXME
+            for (int i = 1; i < k; i += 1) {
+                int toSwap = i;
+                while(toSwap > 0 && array[toSwap] < array[toSwap - 1]) {
+                    swap(array, toSwap, toSwap - 1);
+                    toSwap -= 1;
+                }
+            }
         }
 
         @Override
@@ -60,7 +66,17 @@ public class MySortingAlgorithms {
     public static class SelectionSort implements SortingAlgorithm {
         @Override
         public void sort(int[] array, int k) {
-            // FIXME
+            for (int i = 0; i < k; i += 1) {
+                int minIndex = i;
+                int minEl = array[i];
+                for (int j = i + 1; j < k; j += 1) {
+                    if (Math.min(minEl, array[j]) == array[j]) {
+                        minEl = array[j];
+                        minIndex = j;
+                    }
+                }
+                swap(array, i, minIndex);
+            }
         }
 
         @Override
@@ -77,15 +93,58 @@ public class MySortingAlgorithms {
     public static class MergeSort implements SortingAlgorithm {
         @Override
         public void sort(int[] array, int k) {
-            // FIXME
+            mergeSort(array, 0, k);
+        }
+        private void mergeSort(int[] array, int left, int right) {
+            int mid = (left + right) / 2;
+            mergeSort(array, left, mid);
+            mergeSort(array, mid + 1, right);
+            merge(array, left, mid, right);
         }
 
+        private void merge(int[] array, int left, int mid, int right) {
+            int n1 = mid - left + 1;
+            int n2 = right - mid;
+            int L[] = new int [n1];
+            int R[] = new int [n2];
+            for (int i=0; i<n1; ++i) {
+                L[i] = array[left + i];
+            }
+            for (int j=0; j<n2; ++j) {
+                R[j] = array[mid + 1 + j];
+            }
+            int i = 0, j = 0;
+            int k = left;
+            while (i < n1 && j < n2) {
+                if (L[i] <= R[j]) {
+                    array[k] = L[i];
+                    i++;
+                } else {
+                    array[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+            while (i < n1) {
+                array[k] = L[i];
+                i++;
+                k++;
+            }
+
+            while (j < n2) {
+                array[k] = R[j];
+                j++;
+                k++;
+            }
+        }
         // may want to add additional methods
 
         @Override
         public String toString() {
             return "Merge Sort";
         }
+
+        int[] _toSort;
     }
 
     /**
--- hw7/MySortingAlgorithmsTest.java
+++ hw7/MySortingAlgorithmsTest.java
@@ -14,7 +14,8 @@ public class MySortingAlgorithmsTest {
         // new MySortingAlgorithms.CountingSort(),
         // new MySortingAlgorithms.HeapSort(),
         //new MySortingAlgorithms.QuickSort(),
-        new MySortingAlgorithms.LSDSort()};
+        //new MySortingAlgorithms.LSDSort()
+    };
         //new MySortingAlgorithms.MSDSort()};
 
     private SortingAlgorithm javaSort = new MySortingAlgorithms.JavaSort();
